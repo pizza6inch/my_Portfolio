bd02a89308573deb121f410633b62494fd551b60
Pizza6inch <pizza6inch@gmail.com>
2024-10-01 10:17:02 +0800

    fix start none display

--- src/components/canvas/StarsCanvas.tsx
+++ src/components/canvas/StarsCanvas.tsx
@@ -4,27 +4,25 @@ import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random";
 import * as THREE from "three";
 
-type StarsProps = {
-  [key: string]: any;
-};
-const Stars = (props: StarsProps) => {
+const Stars = () => {
   const ref = useRef<THREE.Points>(null!);
-  const [sphere] = useState(() => {
-    const array = Float32Array.from(
-      random.inSphere(new Float64Array(5000), { radius: 1.2 })
-    );
-    const vector = new THREE.Vector3().fromArray(array);
-    return vector;
-  });
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+
   useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.rotation.x -= delta / 10;
-      ref.current.rotation.y -= delta / 15;
-    }
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
   });
+
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} position={sphere} stride={3} frustumCulled {...props}>
+      <Points
+        ref={ref}
+        positions={sphere as Float32Array}
+        stride={3}
+        frustumCulled
+      >
         <PointMaterial
           transparent
           color="#f272c8"
@@ -36,6 +34,7 @@ const Stars = (props: StarsProps) => {
     </group>
   );
 };
+
 const StarsCanvas = () => {
   return (
     <div className="w-full h-auto absolute inset-0 z-[-1]">
@@ -43,9 +42,11 @@ const StarsCanvas = () => {
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
+
         <Preload all />
       </Canvas>
     </div>
   );
 };
+
 export default StarsCanvas;
