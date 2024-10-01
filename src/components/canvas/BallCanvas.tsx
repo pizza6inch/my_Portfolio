62d7d5d4040222ba918b1ec3fd80d8ab39c08054
huiwuzhang <huiwuzhang@eztravel.com.tw>
2024-10-01 14:33:59 +0800

    debug

--- src/components/canvas/BallCanvas.tsx
+++ src/components/canvas/BallCanvas.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import { Decal, Float, OrbitControls, useTexture } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
